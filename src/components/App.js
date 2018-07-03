be9cb5c02c21f7f1d172739314df14a39f7de105
Ishaan Kolluri <ishaankolluri@gmail.com>
2018-07-03 16:00:42 -0400

    Add individual table fetches

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,54 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import { fetchData } from "../modules/counter";
 import Dashboard from "./Dashboard";
 import SingleView from "./SingleView";
 
-const App = () => (
-  <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-    </header>
+class App extends React.Component {
+  componentWillMount() {
+    console.log("mounting");
+    const items = this.props.fetchData("item");
+    const sessions = this.props.fetchData("session");
+    const responses = this.props.fetchData("response");
+    const contact = this.props.fetchData("contact");
+  }
 
-    <main>
-      <Route exact path="/" component={Dashboard} />
-      <Route exact path="/about-us" component={SingleView} />
-    </main>
-  </div>
-);
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/about-us">About</Link>
+        </header>
+        <main>
+          <Route exact path="/" render={() => <Dashboard {...this.props} />} />
+          <Route
+            exact
+            path="/:itemId"
+            render={() => <SingleView {...this.props} />}
+          />
+        </main>
+      </div>
+    );
+  }
+}
 
-export default App;
+const mapStateToProps = state => state.counter;
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      fetchData
+    },
+    dispatch
+  );
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+// export default App;
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,41 +1,14 @@
 import React from "react";
-import { push } from "react-router-redux";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
-import { fetchData } from "../modules/counter";
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome to Chorus</h1>
-        <p>
-          <button onClick={() => this.props.fetchData()}>
-            Try and fetch data
-          </button>
-        </p>
-        <p>
-          <button onClick={() => this.props.changePage()}>
-            Go to about page via redux
-          </button>
-        </p>
+        <p>This is the dashboard.</p>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => state.counter;
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      fetchData,
-      changePage: () => push("/about-us")
-    },
-    dispatch
-  );
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Dashboard);
+export default Dashboard;
